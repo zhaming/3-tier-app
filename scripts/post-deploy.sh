1fa38d531dec42b98c2d110e4368b416a33e7dd8
Thomas Orozco <thomas@orozco.fr>
2013-03-29 14:42:08 -0700

    Fix post deploy script

--- scripts/post-deploy.sh
+++ scripts/post-deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Remove a pre-existing install
 mkdir -p /usr/lib/cgi-bin
 rm -f /usr/lib/cgi-bin/app.py
-ln -s /var/www/app/app.py /usr/lib/cgi-bin
+
+# Symlink the app to cgi-bin
+cd %remote_path%
+ln -s app/app.py /usr/lib/cgi-bin
