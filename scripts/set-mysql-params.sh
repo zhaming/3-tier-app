b40c07b2f51e8229243b203ca27e989b43a7a09b
Thomas Orozco <thomas@orozco.fr>
2013-04-24 15:22:46 -0700

    Change variable order

--- scripts/set-mysql-params.sh
+++ scripts/set-mysql-params.sh
@@ -2,9 +2,9 @@
 
 mkdir -p /var/config
 
-echo '%writes-endpoint%' > /var/config/mysql-master
-echo '%reads-endpoint%' > /var/config/mysql-slave
 echo '%username%' > /var/config/mysql-username
 echo '%password%' > /var/config/mysql-password
+echo '%writes-endpoint%' > /var/config/mysql-master
+echo '%reads-endpoint%' > /var/config/mysql-slave
 
 echo 'Successfully updated MySQL params.'
