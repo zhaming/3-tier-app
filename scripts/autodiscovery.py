ec5fa7fa8c9582e4f27423f58d9e12838767321c
Thomas Orozco <thomas@orozco.fr>
2014-04-27 17:55:05 +0100

    Improve commenting

--- scripts/autodiscovery.py
+++ scripts/autodiscovery.py
@@ -43,6 +43,10 @@ class FarmRoleParams(object):
 
 class FarmRoleEngine(object):
     def _szradm(self, params):
+        """
+        Make a call to szradm, check for errors, and return the output
+        """
+
         params = copy.copy(params)
         params.insert(0, "/usr/local/bin/szradm")
         proc = subprocess.Popen(params, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -51,6 +55,13 @@ class FarmRoleEngine(object):
         return ElementTree.parse(proc.stdout)
 
     def _get_farm_roles(self, behaviour):
+        """
+        Retrieve the list of Farm Roles matching a selected behaviour in the
+        current Farm
+
+        Internally, this invokes szradm -q list-roles and parses the XML
+        """
+
         args = ["-q", "list-roles"]
         t = self._szradm(args)
         all_roles = t.find("roles")
@@ -64,6 +75,14 @@ class FarmRoleEngine(object):
         return matching_roles
 
     def _get_farm_role(self, behaviour):
+        """
+        Retrieve the Farm Role matching a selected behaviour in the
+        current Farm
+
+        This function will fail if multiple Farm Roles match the same
+        behaviour
+        """
+
         matching_roles = self._get_farm_roles(behaviour)
 
         if not matching_roles:
@@ -75,15 +94,35 @@ class FarmRoleEngine(object):
             return role
 
     def get_farm_role_id(self, behaviour):
+        """
+        Get the ID for the Farm Role matching a selected behaviour in the
+        current Farm
+
+        This function will fail if multiple Farm Roles match the same
+        behaviour
+        """
         role = self._get_farm_role(behaviour)
         return int(role.attrib["id"])
 
     def get_farm_role_hosts(self, behaviour):
+        """
+        Get a list of Hosts for the Farm Role matching a selected behaviour
+        in the current Farm
+
+        Returns a list of dicts. Each dict represents a host
+
+        This function will fail if multiple Farm Roles match the same
+        behaviour
+        """
         role = self._get_farm_role(behaviour)
         hosts = role.find("hosts")
         return [host.attrib for host in hosts.findall("host")]
 
     def get_farm_role_params(self, farm_role_id):
+        """
+        Returns the Role parameters for the Farm Role matching the selected ID
+        in the current Farm
+        """
         params = ["-q", "list-farm-role-params", "farm-role-id={0}".format(farm_role_id)]
         return FarmRoleParams(self._szradm(params))
 
@@ -91,21 +130,37 @@ class FarmRoleEngine(object):
 def prepare_config_files(engine):
     mysql_role = "mysql2"
 
+    # Retrieve the ID of the MySQL Role
     mysql_role_id = engine.get_farm_role_id(mysql_role)
+
+    # Retrieve the list of MySQL hosts
     mysql_hosts = engine.get_farm_role_hosts(mysql_role)
+
+    # Retrieve MySQL configuration
     mysql_params = engine.get_farm_role_params(mysql_role_id)
 
+    # Create configuration files
     files = []
+
+    # Start with MySQL configuration
+    # We're using the Scalr admin user to access the database
+    # Note: this is pretty unsafe. Don't use this in production - this is only
+    # an example
     files.append(("mysql-username", "scalr"))
     files.append(("mysql-password", mysql_params.mysql2.root_password))
 
+    # Create a filter function that lets us parse through the list of MySQL
+    # servers and tell the master from the slaves
     def hosts_by_replication(replicating):
         replicating = "1" if replicating else "0"
         f = lambda host: host["replication-master"] == replicating
         hosts = filter(f, mysql_hosts)
         return "\n".join([host["internal-ip"] for host in hosts])
 
+    # Create a configuration file with the MySQL master's IP
     files.append(("mysql-master", hosts_by_replication(True)))
+
+    # Add a configuration file with MySQL slave IPs
     files.append(("mysql-slave", hosts_by_replication(False)))
 
     return files
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,13 +4,18 @@ set -o errexit
 set -o nounset
 
 
-# Set Variables
+# Set default configuration parameters
 
 DEFAULT_DEPLOY_PATH=/opt/scalr/webapp
 DEFAULT_APP_REPO=https://github.com/scalr-tutorials/3-tier-app.git
 DEFAULT_APP_BRANCH=master
 DEFAULT_PID_FILE=/var/run/webapp.pid
 DEFAULT_PORT=8000
+
+
+# Use configuration from the environment if provided
+# Fall back to defaults if no configuration is provided
+# Note: environment variables can be provided using Scalr Global Variables
 
 : ${DEPLOY_PATH:="$DEFAULT_DEPLOY_PATH"}
 : ${APP_REPO:="$DEFAULT_APP_REPO"}
@@ -19,7 +24,8 @@ DEFAULT_PORT=8000
 : ${PORT:="$DEFAULT_PORT"}
 
 
-# Install System Dependencies
+# Install system dependencies
+# Check operating system first
 
 if [ -f /etc/debian_version ]; then
   OS=debian
@@ -34,13 +40,15 @@ else
 fi
 
 
-# Instal Python Dependencies
+# Instal python dependencies
+# This is platform-independent
 
 easy_install pip
 pip install flask gunicorn
 
 
-# Install or Update
+# If the app hasn't been deployed yet, create the path, and deploy the app
+# If the app had been deployed before, update it
 
 if [ ! -d "$DEPLOY_PATH" ]; then
   mkdir -p $DEPLOY_PATH
@@ -53,12 +61,15 @@ else
 fi
 
 
-# Kill existing process, if it exists
+# If any existing app processes are alive, kill them
+
 kill `cat $PID_FILE 2>/dev/null` || true
 
-# Launch our process
+
+# Launch the app
 gunicorn --chdir $DEPLOY_PATH/app --daemon --pid $PID_FILE --bind 0.0.0.0:$PORT \
   --access-logfile /var/log/webapp.access.log --error-logfile /var/log/webapp.error.log \
   web:application
 
-echo "Started Webapp"
+
+echo "Started Webapp - listening on $PORT"
